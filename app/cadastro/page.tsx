d94e06a0dd15cda983d43792148b3f91e88e9c81
guirra-byte <guirramatheus1@gmail.com>
2025-04-24 22:06:01 -0300

    update: ad like middleware to ensure user is authorized to access register page resources

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,9 +1,17 @@
+import { allowedEmails } from "@/can-access";
 import Cadastro from "@/components/cadastro";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function CadastroPage() {
   const session = await auth();
-  if (!session?.user) redirect("auth/login");
+  if (session) {
+    if (!session.user || !session.user.email) redirect("/unauthorized");
+    const entireUserCanAccess = allowedEmails(session.user.email);
+    if (!entireUserCanAccess) {
+      redirect("/unauthorized");
+    }
+  }
+
   return <Cadastro />;
 }
