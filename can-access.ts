c7359725adc37d1c5238b982431175ec1e4da4b1
guirra-byte <guirramatheus1@gmail.com>
2025-04-24 23:04:48 -0300

    fix: putting allowed emails locally

--- can-access.ts
+++ can-access.ts
@@ -1,8 +1,5 @@
+const emails = ["Realizandosonhos05@gmail.com", "guirramatheus1@gmail.com"];
 export const allowedEmails = (email?: string | null) => {
-  if (!process.env.ALLOWED_EMAILS) return false;
-
-  const emails = process.env.ALLOWED_EMAILS.split(",");
   const isAllowed = emails.find((_email) => _email === email);
-
   return isAllowed ? true : false;
 };
