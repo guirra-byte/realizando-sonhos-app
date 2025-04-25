a2ff3206dafb6dc26043a294cb1b69ee66110002
guirra-byte <guirramatheus1@gmail.com>
2025-04-24 21:59:15 -0300

    update: switching auth strategy using email validation

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,22 @@
+import { allowedEmails } from "@/can-access";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    signIn: async ({ user }) => {
+      if (!user.email) return false;
+
+      const canAccess = allowedEmails(user.email);
+      if (!canAccess) return false;
+      return true;
+    },
+  },
   pages: { signIn: "/auth/login" },
 });
