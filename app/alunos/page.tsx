3799b79b717a5448fec7abf59e443308cbc16748
guirra-byte <guirramatheus1@gmail.com>
2025-04-24 22:01:40 -0300

    update: add like a middleware to ensure user is authorized to access that page resource

--- app/alunos/page.tsx
+++ app/alunos/page.tsx
@@ -1,9 +1,17 @@
 import AlunosPage from "@/components/alunos";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
-
+import { allowedEmails } from "@/can-access";
 export default async function Alunos() {
   const session = await auth();
+  if (session) {
+    if (!session.user || !session.user.email) redirect("/unauthorized");
+    const entireUserCanAccess = allowedEmails(session.user.email);
+    if (!entireUserCanAccess) {
+      redirect("/unauthorized");
+    }
+  }
+
   if (!session?.user) redirect("/auth/login");
   return <AlunosPage user={session.user} />;
 }
--- app/turmas/page.tsx
+++ app/turmas/page.tsx
@@ -1,8 +1,17 @@
+import { allowedEmails } from "@/can-access";
 import ClassManagement from "@/components/turmas/class-management";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 export default async function Turmas() {
   const session = await auth();
-  if (!session?.user) redirect("/auth/login");
+
+  if (session) {
+    if (!session.user || !session.user.email) redirect("/unauthorized");
+    const entireUserCanAccess = allowedEmails(session.user.email);
+    if (!entireUserCanAccess) {
+      redirect("/unauthorized");
+    }
+  }
+
   return <ClassManagement />;
 }
