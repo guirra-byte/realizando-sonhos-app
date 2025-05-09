84c94b90fb8d63eb1de6516c57ca9010f957b846
guirra-byte <guirramatheus1@gmail.com>
2025-05-09 10:18:07 -0300

    fix(turmas): redirect /turmas request to /alunos

--- app/turmas/page.tsx
+++ app/turmas/page.tsx
@@ -2,16 +2,18 @@ import { allowedEmails } from "@/can-access";
 import ClassManagement from "@/components/turmas/class-management";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+
 export default async function Turmas() {
-  const session = await auth();
+  redirect("/alunos");
+  // const session = await auth();
 
-  if (session) {
-    if (!session.user || !session.user.email) redirect("/unauthorized");
-    const entireUserCanAccess = allowedEmails(session.user.email);
-    if (!entireUserCanAccess) {
-      redirect("/unauthorized");
-    }
-  }
+  // if (session) {
+  //   if (!session.user || !session.user.email) redirect("/unauthorized");
+  //   const entireUserCanAccess = allowedEmails(session.user.email);
+  //   if (!entireUserCanAccess) {
+  //     redirect("/unauthorized");
+  //   }
+  // }
 
-  return <ClassManagement />;
+  // return <ClassManagement />;
 }
