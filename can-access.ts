46be0c05644b9d609fbfa684542c7d12c79bb435
guirra-byte <guirramatheus1@gmail.com>
2025-05-16 09:14:08 -0300

    fix(access): fix allowed emails

--- can-access.ts
+++ can-access.ts
@@ -1,4 +1,4 @@
-const emails = ["Realizandosonhos05@gmail.com", "guirramatheus1@gmail.com"];
+const emails = ["realizandosonhos05@gmail.com", "guirramatheus1@gmail.com"];
 export const allowedEmails = (email?: string | null) => {
   const isAllowed = emails.find((_email) => _email === email);
   return isAllowed ? true : false;
