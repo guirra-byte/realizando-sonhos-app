44ce152da775606dae7f0158f786b2690b901c06
guirra-byte <guirramatheus1@gmail.com>
2025-05-16 10:26:32 -0300

    update(access): add new email to acess application

--- can-access.ts
+++ can-access.ts
@@ -1,4 +1,4 @@
-const emails = ["realizandosonhos05@gmail.com", "guirramatheus1@gmail.com"];
+const emails = ["realizandosonhos05@gmail.com", "guirramatheus1@gmail.com", "371544jm@gmail.com"];
 export const allowedEmails = (email?: string | null) => {
   const isAllowed = emails.find((_email) => _email === email);
   return isAllowed ? true : false;
