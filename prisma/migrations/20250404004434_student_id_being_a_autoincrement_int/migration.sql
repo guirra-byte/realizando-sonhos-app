027fbe9c9cd173e8335d3567f34c4ce3f3a0748c
guirra-byte <guirramatheus1@gmail.com>
2025-05-28 10:13:20 -0300

    fix(db): fixing db issue that cant access supabase database in production env, creating Vercel direct integration with supabase

--- prisma/migrations/20250404004434_student_id_being_a_autoincrement_int/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE students_id_seq;
-ALTER TABLE "students" ALTER COLUMN "id" SET DEFAULT nextval('students_id_seq');
-ALTER SEQUENCE students_id_seq OWNED BY "students"."id";
--- prisma/migrations/20250521193817_add_invite_request_owner_property/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `invited_by` to the `allowed_users` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "allowed_users" ADD COLUMN     "invited_by" TEXT NOT NULL;
